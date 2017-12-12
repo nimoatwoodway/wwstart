8e5ee0f5012314a7b8bb0752b70ac78bb7cbfe6c
nimoatwoodway <chris@wunderwerk.io>
2017-12-12 16:10:27 +0100

    changes

--- setup.sh
+++ setup.sh
@@ -1,8 +1,30 @@
-#/bin/bash
+#!/bin/bash
 
 #set some constants
 root="${PWD}"
 
 #create some dirs
 mkdir -p $root/mysql
-mkdir -p $root/public
+mkdir -p $root/docroot
+
+#create .gitignore if missing
+if [ -e $root/.gitignore ]
+then
+  printf ".gitignore already exists\n"
+else
+  touch $root/.gitignore
+  printf ".gitignore created\n"
+fi
+
+#add some lines to .gitignore, but only if we didnt done it before
+if grep -q "wwstart" "$root/.gitignore";
+then
+  printf ".gitignore already customized\n"
+else
+  cmd="printf '\n#wwstart ignore stuff\n#from setup.sh\n#ignore docker\ndocker\n#macOS\n.DS_Store\n#PhpStorm configs\n.idea' >> $root/.gitignore"
+  if eval $cmd; then
+    printf ".gitignore customized\n"
+  else
+    printf ".gitignore write error\n"
+  fi
+fi
